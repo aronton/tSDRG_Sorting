6b7fc45dc0cc90e38c6e6af5bacd3f52948fb33a
aronton <aronton0502@gmail.com>
2022-11-10 13:58:24 +0800

    remove comment

--- bulk_corr_sub.sh
+++ bulk_corr_sub.sh
@@ -11,39 +11,13 @@ echo "chi        ==> ${4}"
 echo "L        ==> ${5}"
 echo "jdis         ==> ${6}"
 echo "dim         ==> ${7}"
-echo "Seed         ==> ${8}"
+echo "init_Seed         ==> ${8}"
 echo "final_Seed        ==> ${9}"
-# echo "s1        ==> ${10}"
-# echo "s2        ==> ${11}"
-# echo "Nseed        ==> ${12}"
-# echo "Spin         ==> ${13}"
 
 
 date
-    #echo "L:" ${1}~${2}"("${3}")"; "J:" ${4}~${5}"("${6}")"; "D:" ${7}~${8}"("${9}")"; "seed:" ${10}~${11}"("${12}")"; >> /home/aronton/tSDRG_project/Sorting_data/Spin1/record
-    python /home/aronton/tSDRG_project/Sorting_data/bulk_corr.py ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}
+    python /home/aronton/tSDRG_project/Sorting_data/tSDRG_Sorting/bulk_corr.py ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}
 echo -e "\ndone.\n\n"
 date
 
 
-# [ "$#" -lt 13 ] && echo "The number of parameter is less than 13.  Stop here." && exit 0
-# echo "L1         ==> ${1}"
-# echo "L2         ==> ${2}"
-# echo "space_L         ==> ${3}"
-# echo "J1        ==> ${4}"
-# echo "J2        ==> ${5}"
-# echo "space_J         ==> ${6}"
-# echo "D1         ==> ${7}"
-# echo "D2         ==> ${8}"
-# echo "space_D        ==> ${9}"
-# echo "s1        ==> ${10}"
-# echo "s2        ==> ${11}"
-# echo "Nseed        ==> ${12}"
-# echo "Spin         ==> ${13}"
-
-
-# date
-#     #echo "L:" ${1}~${2}"("${3}")"; "J:" ${4}~${5}"("${6}")"; "D:" ${7}~${8}"("${9}")"; "seed:" ${10}~${11}"("${12}")"; >> /home/aronton/tSDRG_project/Sorting_data/Spin1/record
-#     python /home/aronton/tSDRG_project/Sorting_data/Spin15/ZL_Metadatacopy.py ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13}
-# echo -e "\ndone.\n\n"
-# date
