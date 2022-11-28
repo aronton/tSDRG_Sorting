1090946b1fe40ac0c7f9433ace665534404a610d
aronton <aronton0502@gmail.com>
2022-11-28 17:22:54 +0800

    para and RMpara sub,sh

--- RMparameterSub.sh
+++ RMparameterSub.sh
@@ -15,14 +15,11 @@ read -p "Space_J : " space_J
 read -p "Init_D : " D1
 read -p "Final_D : " D2
 read -p "Space_D : " space_D
-read -p "seed1 : " s1
-read -p "seed2 : " s2
-read -p "Nseed : " Nseed
 read -p "BC : " BC
 read -p "deltaSeed : " deltaSeed
 
-echo "Partition:" ${partition} "Number of core:" ${cpuspertask} "Spin:${Spin}"";L:"${L1}"~" ${L2} "(" ${space_L} ")" ";J:" ${J1} "~" ${J2} "(" ${space_J} ")" ";D:" ${D1} "~" ${D2} "(" ${space_D} ")" ",seed1:" ${s1} ",seed2:" ${s2} "Orderparameter:" ${orderparameter}
+echo "RM_empty:Partition:" ${partition} "Number of core:" ${cpuspertask} "Spin:${Spin}"";L:"${L1}"~" ${L2} "(" ${space_L} ")" ";J:" ${J1} "~" ${J2} "(" ${space_J} ")" ";D:" ${D1} "~" ${D2} "(" ${space_D} ")" ",seed1:" ${s1} ",seed2:" ${s2} "Orderparameter:" ${orderparameter}
 
 read -p "Continue or not (Y/N): " Continue
 
-nohup sh /home/aronton/tSDRG_project/Sorting_data/tSDRG_Sorting/${orderparameter}/Metasub_${orderparameter}.sh $orderparameter $partition $Ncore $Spin $P $bonDim $L1 $L2 $space_L $J1 $J2 $space_J $D1 $D2 $space_D $s1 $s2 $Nseed $BC $deltaSeed&
+nohup sh /home/aronton/tSDRG_project/Sorting_data/tSDRG_Sorting/rm_empty/rm_empty.sh $orderparameter $partition $Ncore $Spin $P $bonDim $L1 $L2 $space_L $J1 $J2 $space_J $D1 $D2 $space_D $BC $deltaSeed&
